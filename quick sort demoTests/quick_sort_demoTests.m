61fb14a44eed913e0137b0abfd3d7191a62babe9
bradleypj@icloud.com <Brad@Bradleys-MacBook-Pro.local>
2014-05-19 18:12:23 -0700

    Initial Commit

---
+++ quick sort demoTests/quick_sort_demoTests.m
@@ -0,0 +1,34 @@
+//
+//  quick_sort_demoTests.m
+//  quick sort demoTests
+//
+//  Created by Bradley Johnson on 5/19/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface quick_sort_demoTests : XCTestCase
+
+@end
+
+@implementation quick_sort_demoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
